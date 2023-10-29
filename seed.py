f0eeeb1b07d33137d7ab5e09f2cb86c904150ba5
Victor <wanjala94vic@gmail.com>
2023-10-29 22:23:26 +0300

    updating server

--- seed.py
+++ seed.py
@@ -18,7 +18,11 @@ with app.app_context():
     restaurants_data = [
         {"name": "Sottocasa NYC", "address": "298 Atlantic Ave, Brooklyn, NY 11201"},
         {"name": "PizzArte", "address": "69 W 55th St, New York, NY 10019"},
-    ]
+        {"name": "Joe's Pizzeria", "address": "123 Main St, New York, NY 10001"},
+        {"name": "Pizza Palace", "address": "456 Elm St, Brooklyn, NY 11202"},
+        {"name": "Mama Mia's Pizzeria", "address": "789 Oak St, Queens, NY 11301"},
+    
+]
     
     for data in restaurants_data:
         restaurant = Restaurant(name=data["name"], address=data["address"])
@@ -32,7 +36,12 @@ with app.app_context():
     pizzas_data = [
         {"name": "Cheese", "ingredients": "Dough, Tomato Sauce, Cheese"},
         {"name": "Pepperoni", "ingredients": "Dough, Tomato Sauce, Cheese, Pepperoni"},
-    ]
+        {"name": "Margherita", "ingredients": "Dough, Tomato Sauce, Mozzarella, Basil"},
+        {"name": "Vegetarian", "ingredients": "Dough, Tomato Sauce, Cheese, Bell Peppers, Mushrooms, Olives"},
+        {"name": "Hawaiian", "ingredients": "Dough, Tomato Sauce, Cheese, Ham, Pineapple"},
+   
+]
+
 
     for data in pizzas_data:
         pizza = Pizza(name=data["name"], ingredients=data["ingredients"])
